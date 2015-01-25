577db77d6c99c60a926e0d89c1c8f0bdb2bc5487
Xiao Shen <xiaoshen2009@gmail.com>
2015-01-25 04:00:23 -0500

    redirect to library page with btn

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,12 @@
 class MessagesController < ApplicationController
 	def create
-		@message = Message.new(params[:message])
+		@message = Message.new(message_params)
 		@message.save!
+		# redirect_to 'welcome_url'
 	end
+private
+  	def message_params
+      params.require(:message).permit(:message, :email, :name)
+    end
+
 end
